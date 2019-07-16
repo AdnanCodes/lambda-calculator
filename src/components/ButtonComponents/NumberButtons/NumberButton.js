7d86fd78a5bdc742366910d1db5945830a1fe54c
Adnan <adnan.chowdhury@outlook.com>
2019-07-16 18:01:17 -0500

    Trying to incoporate the ability to display props via button clicks to display component

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Display from "../../DisplayComponents/Display"
 
 const NumberButton = (props) => {
+
   return (
     <button className = "numberButton">
       {/* Display a button element rendering the data being passed down from the parent container on props */props}
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Display = () => {
-  return <div className ="displayBox">{/* Display any props data here */}0</div>;
+
+const Display = (props) => {
+  const [displayNumbers, setdisplayNumbers] = useState(0)
+  return <div className ="displayBox">{/* Display any props data here */}</div>;
 };
 
 export default Display;
